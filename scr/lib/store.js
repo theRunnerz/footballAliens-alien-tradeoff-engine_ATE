f1a453a47210fd1b3bf8cf2a5aae913f2eb5d193
Lilstarburst <corb.pratt@gmail.com>
2025-12-23 00:08:05 -0700

    Update store.js

--- scr/lib/store.js
+++ scr/lib/store.js
@@ -1,3 +1,15 @@
 import { writable } from 'svelte/store';
 
-export const history = writable([]);
+function persistent(key, initial) {
+  const stored = localStorage.getItem(key);
+  const store = writable(stored ? JSON.parse(stored) : initial);
+
+  store.subscribe(value => {
+    localStorage.setItem(key, JSON.stringify(value));
+  });
+
+  return store;
+}
+
+export const memory = persistent('alien-memory', []);
+export const streak = persistent('alien-streak', 0);
