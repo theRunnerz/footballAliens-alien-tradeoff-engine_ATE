6a5124b6893b7ee8345017e63ec8e66e3104e7ee
Lilstarburst <corb.pratt@gmail.com>
2025-12-23 00:02:05 -0700

    Update +layout.svelte

--- scr/routes/+layout.svelte
+++ scr/routes/+layout.svelte
@@ -1,10 +1,24 @@
-<slot />
+<svelte:head>
+  <title>Football Aliens 👽</title>
+</svelte:head>
+
+<div class="app">
+  <slot />
+</div>
 
 <style>
-  body {
+  :global(body) {
     margin: 0;
-    background: #05080f;
-    color: white;
-    font-family: system-ui, sans-serif;
+    background: radial-gradient(circle at top, #0b1020, #02030a);
+    color: #eafff1;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont;
+  }
+
+  .app {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
   }
 </style>
