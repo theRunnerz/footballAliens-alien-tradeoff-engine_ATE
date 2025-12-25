934bac46a085f5d03c2ddebe2f7873bfc1957584
Lilstarburst <corb.pratt@gmail.com>
2025-12-25 14:38:40 -0700

    Update alienEngine.js

--- alienEngine.js
+++ alienEngine.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const TRIAL_DAYS = 7;
   const FBA_TOKEN_ADDRESS = "TNW5ABkp3v4jfeDo1vRVjxa3gtnoxP3DBN";
   const FBA_REQUIRED = 420;
-  const BACKEND_URL = "https://football-aliens-ai-backend-61l6-lqtevjrqu-runnerzs-projects.vercel.app/api/alien";
+  const BACKEND_URL = "https://football-aliens-ai-backend-e3gj-o5x069hw0-runnerzs-projects.vercel.app/api/alien";
 
   /* ======================
      STATE
